418dc31ebbe279eef6124877720490108fd18861
Bora Ecer <boraecer97@gmail.com>
2018-04-20 22:10:32 +0300

    Datamuse added

---
+++ Compact-Mind/src/UI/CellBlock.java
@@ -0,0 +1,11 @@
+package UI;
+
+public class CellBlock {
+	public String questionNo;
+	public String currentLetter;
+	
+	public CellBlock(){
+		questionNo = ""; // -1 IS NOT A QUESTION START BLOCK
+		currentLetter = ""; // - is for no current letter
+	}
+}
---
+++ Compact-Mind/src/backend/Checkers.java
@@ -0,0 +1,9 @@
+package backend;
+
+public class Checkers 
+{
+	public static boolean isGoogleChecked = false;
+	public static boolean isDatamuseChecked = false;
+	public static int count = 0;
+    public static int count1 = 0;
+}
